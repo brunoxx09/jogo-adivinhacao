c72778a50578f572e5c9ca27b739cf1f75cc517c
Bruno Oliveira <brunobrigida2327@gmail.com>
2026-04-09 23:07:50 -0300

    calculadora de imc

--- jogo de adivinhação.py
+++ jogo de adivinhação.py
@@ -6,9 +6,9 @@ while True:
     palpite = int(input("Digite seu palpite: "))
 
     if palpite > numero_sorteado:
-        print("Alto demais! Tente um número menor.")
+        print("tente um número menor.")
     elif palpite < numero_sorteado:
-        print("Baixo demais! Tente um número maior.")
+        print("tente um número maior.")
     else:
-        print("Parabéns, você acertou!")
+        print("você acertou")
         break
